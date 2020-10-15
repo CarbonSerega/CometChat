c534bb424760b4ad25b51e566b3af851b6a638b2
seregacarbon <ekvaserega@gmail.com>
2020-10-16 02:06:28 +0300

    v if check if the image is not null

--- comet-chat/layouts/default.vue
+++ comet-chat/layouts/default.vue
@@ -13,7 +13,7 @@
           </v-list-item-content>
 
           <v-list-item-icon>
-            <img class="user-avatar" :src="u.img" :alt="u.id">
+            <img class="user-avatar" v-if="u.img" :src="u.img" :alt="u.id">
           </v-list-item-icon>
         </v-list-item>
       </v-list>
